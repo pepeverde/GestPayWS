569efddd83ac0de25f07083c5e49207aa7319da2
cristianp6 <cristianp6@gmail.com>
2015-12-23 12:42:27 +0100

    Added CheckCard, VerifyCard, Delete, Refund and Settle parameter classes

---
+++ src/Parameter/SettleParameter.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+ * This file is part of the GestPayWS library.
+ *
+ * (c) Cristian Pascottini <cristianp6@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EndelWar\GestPayWS\Parameter;
+
+use InvalidArgumentException;
+
+/**
+ * Class SettleParameter
+ * @package EndelWar\GestPayWS\Parameter
+ *
+ * @property string $shopLogin
+ * @property string $shopTransactionId;
+ * @property string $amount;
+ * @property string $uicCode;
+ * @property string $bankTransID;
+ * @property string $requestToken;
+ * @property string $tokenValue;
+ */
+class SettleParameter extends Parameter
+{
+    protected $parametersName = array(
+      'shopLogin',
+      'shopTransactionId',
+      'amount',
+      'uicCode',
+      'bankTransID',
+      'requestToken',
+      'tokenValue',
+    );
+    protected $mandatoryParameters = array(
+      'shopLogin',
+      'shopTransactionId',
+      'amount',
+      'uicCode',
+      'bankTransID',
+    );
+    private $invalidChars = array(
+        '&',
+        ' ',
+        '§', //need also to be added programmatically, because UTF-8
+        '(',
+        ')',
+        '*',
+        '<',
+        '>',
+        ',',
+        ';',
+        ':',
+        '*P1*',
+        '/',
+        '[',
+        ']',
+        '?',
+        '=',
+        '--',
+        '/*',
+        '%',
+        '//',
+    );
+    private $invalidCharsFlattened = '';
+
+    /**
+     * @param array $parameters
+     */
+    public function __construct(array $parameters = array())
+    {
+        $this->invalidChars[] = chr(167); //§ ascii char
+
+        parent::__construct($parameters);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        if (!in_array($key, $this->parametersName, true)) {
+            throw new InvalidArgumentException(sprintf('%s is not a valid parameter name.', $key));
+        }
+        $this->verifyParameterValidity($value);
+        parent::set($key, $value);
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function verifyParameterValidity($value)
+    {
+        if (strlen($this->invalidCharsFlattened) === 0) {
+            $invalidCharsQuoted = array_map('preg_quote', $this->invalidChars);
+            $this->invalidCharsFlattened = implode('|', $invalidCharsQuoted);
+        }
+
+        if (preg_match_all('#' . $this->invalidCharsFlattened . '#', $value, $matches)) {
+            $invalidCharsMatched = '"' . implode('", "', $matches[0]) . '"';
+            throw new InvalidArgumentException(
+                'String ' . $value . ' contains invalid chars (i.e.: ' . $invalidCharsMatched . ').'
+            );
+        }
+
+        return true;
+    }
+}
