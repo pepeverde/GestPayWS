8e0b08b0667d2c09b3420c218a4d771464939bfa
cristianp6 <cristianp6@gmail.com>
2015-12-23 12:45:18 +0100

    Added checkCard, verifyCard, delete, refund and settle methods in WSS2S

--- src/WSS2S.php
+++ src/WSS2S.php
@@ -12,8 +12,18 @@
 namespace EndelWar\GestPayWS;
 
 use EndelWar\GestPayWS\Parameter\PagamParameter;
+use EndelWar\GestPayWS\Parameter\CheckCardParameter;
+use EndelWar\GestPayWS\Parameter\VerifyCardParameter;
+use EndelWar\GestPayWS\Parameter\SettleParameter;
+use EndelWar\GestPayWS\Parameter\RefundParameter;
+use EndelWar\GestPayWS\Parameter\DeleteParameter;
 use EndelWar\GestPayWS\Parameter\RequestTokenParameter;
 use EndelWar\GestPayWS\Response\PagamResponse;
+use EndelWar\GestPayWS\Response\CheckCardResponse;
+use EndelWar\GestPayWS\Response\VerifyCardResponse;
+use EndelWar\GestPayWS\Response\SettleResponse;
+use EndelWar\GestPayWS\Response\RefundResponse;
+use EndelWar\GestPayWS\Response\DeleteResponse;
 use EndelWar\GestPayWS\Response\RequestTokenResponse;
 
 /**
@@ -31,6 +41,61 @@ class WSS2S extends Service
         $this->validateParameters($parameters);
         $soapResponse = $this->soapClient->callPagamS2S($parameters);
         return new PagamResponse($soapResponse);
+    }
+
+    /**
+     * @param CheckCardParameter $parameters
+     * @return EncryptResponse
+     */
+    public function checkCard(CheckCardParameter $parameters)
+    {
+        $this->validateParameters($parameters);
+        $soapResponse = $this->soapClient->callCheckCartaS2S($parameters);
+        return new CheckCardResponse($soapResponse);
+    }
+
+    /**
+     * @param VerifyCardParameter $parameters
+     * @return EncryptResponse
+     */
+    public function verifyCard(VerifyCardParameter $parameters)
+    {
+        $this->validateParameters($parameters);
+        $soapResponse = $this->soapClient->callVerifyCardS2S($parameters);
+        return new VerifyCardResponse($soapResponse);
+    }
+
+    /**
+     * @param SettleParameter $parameters
+     * @return EncryptResponse
+     */
+    public function settle(SettleParameter $parameters)
+    {
+        $this->validateParameters($parameters);
+        $soapResponse = $this->soapClient->callSettleS2S($parameters);
+        return new SettleResponse($soapResponse);
+    }
+
+    /**
+     * @param RefundParameter $parameters
+     * @return EncryptResponse
+     */
+    public function refund(RefundParameter $parameters)
+    {
+        $this->validateParameters($parameters);
+        $soapResponse = $this->soapClient->callRefundS2S($parameters);
+        return new RefundResponse($soapResponse);
+    }
+
+    /**
+     * @param DeleteParameter $parameters
+     * @return EncryptResponse
+     */
+    public function delete(DeleteParameter $parameters)
+    {
+        $this->validateParameters($parameters);
+        $soapResponse = $this->soapClient->callDeleteS2S($parameters);
+        return new DeleteResponse($soapResponse);
     }
 
     /**
